ab0ebc8753f2b95cacf5d1f3c279ac87498fa022
Nicolas Ramirez <niico.eedu@gmail.com>
2022-11-25 01:06:16 -0300

    se agrega comentarios al test

--- test/test_log_in.py
+++ test/test_log_in.py
@@ -5,11 +5,16 @@ from src.pages.log_in_page import LogInPage
 def test_browserstack():
     driver = webdriver.Chrome()
     driver.get("https://www.saucedemo.com/")
+    driver.maximize_window()
 
     log_in_page = LogInPage(driver)
 
+    # Se ingresa las credenciales
     log_in_page.select_username()
     log_in_page.select_password()
+    # Se seleccion el boton de log in
     log_in_page.click_login()
+    # Se valida el inicio de sesion con una imagen
+    log_in_page.validate_success()
 
     driver.quit()
