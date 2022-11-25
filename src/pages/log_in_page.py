c56489697fd03ab7d62cca28609633700dd3b13d
Nicolas Ramirez <niico.eedu@gmail.com>
2022-11-25 01:05:52 -0300

    se agrega comentarios al page

--- src/pages/log_in_page.py
+++ src/pages/log_in_page.py
@@ -6,12 +6,15 @@ class LogInPage(PageFactory):
     def __init__(self, driver):
         self.driver = driver
 
+    # Selectores
     locators = {
         'user_name': ('CSS', '#user-name'),
         'password': ('CSS', '#password'),
-        'login_btn': ('CSS', '#login-button')
+        'login_btn': ('CSS', '#login-button'),
+        'validation': ('CSS', '#item_4_img_link > img')
     }
 
+    # Funciones del page
     def select_username(self):
         self.user_name.set_text('standard_user\n')
         time.sleep(2)
@@ -22,3 +25,6 @@ class LogInPage(PageFactory):
     def click_login(self):
         self.login_btn.click()
         time.sleep(2)
+
+    def validate_success(self):
+        self.validation.visibility_of_element_located()
